ead3d07758b2807191b26163915bc81349eabc6a
Florencia Quinteros <56704793+fl0rchus@users.noreply.github.com>
2020-11-23 18:10:29 -0300

    Update mis-gifos.js

--- js/mis-gifos.js
+++ js/mis-gifos.js
@@ -3,10 +3,10 @@ const btnSwitch = document.querySelector("#switch");
 const logo = document.querySelector(".logo");
 btnSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark");
-  logo.src = "../assets/logo-desktop-modo-noc.svg";
+  logo.src = "assets/logo-desktop-modo-noc.svg";
   btnSwitch.textContent = "MODO DIURNO";
   if (document.body.classList == "") {
-    logo.src = "../assets/logo-desktop.svg";
+    logo.src = "assets/logo-desktop.svg";
     btnSwitch.textContent = "MODO NOCTURNO";
   }
 });
