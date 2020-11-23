e7454b151100dfbb2c273ef2194f642b3a562b1d
Florencia Quinteros <56704793+fl0rchus@users.noreply.github.com>
2020-11-23 17:54:24 -0300

    retraso de aparicion de navbar al hacer scroll

--- js/navbar.js
+++ js/navbar.js
@@ -1,21 +1,22 @@
 //Variables
-const navSearch = document.getElementById('nav-search');
-const shadow = document.getElementById('header');
-const plus = document.getElementById('plus');
+const navSearch = document.getElementById("nav-search");
+const shadow = document.getElementById("header");
+const plus = document.getElementById("plus");
 
-//SCROLL 
-window.addEventListener('DOMContentLoaded', () =>{
-    shadow.classList.remove('shadow');
-})
-window.addEventListener('scroll', () => {
-    const scrollPX = window.scrollY;
-     if(scrollPX > 20){
-         navSearch.style.visibility = 'visible';
-         shadow.classList.add('shadow');
-         plus.style.visibility = 'hidden'; 
-     }else{
-        navSearch.style.visibility = 'hidden';
-        plus.style.visibility = 'visible';
-        shadow.classList.remove('shadow');
-     }
+//SCROLL
+window.addEventListener("DOMContentLoaded", () => {
+  shadow.classList.remove("shadow");
+});
+window.addEventListener("scroll", () => {
+  const scrollPX = window.scrollY;
+  console.log(scrollPX);
+  if (scrollPX > 450) {
+    navSearch.style.visibility = "visible";
+    shadow.classList.add("shadow");
+    plus.style.visibility = "hidden";
+  } else {
+    navSearch.style.visibility = "hidden";
+    plus.style.visibility = "visible";
+    shadow.classList.remove("shadow");
+  }
 });
